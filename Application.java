178ab8d774e5ec62bd5e22d21fb109ea00b43440
Paul Friedli <paul.friedli@edufr.ch>
2026-08-27 17:46:15 +0200

    Initial commit

---
+++ Application.java
@@ -0,0 +1,30 @@
+public class Application {
+
+    // -----------------------------------------------------------
+    // Sujets : Méthodes (+sujets E0c)
+    // -----------------------------------------------------------
+
+    public static void main(String[] args) {
+
+        // Écrivez une méthode qui génère un tableau rempli de nombres entiers
+        // aléatoires. Cette méthode recevra le nombre de valeurs à générer, ainsi que
+        // les limites de ces nombres (par exemple de -5 à +5).
+
+        // Écrivez une méthode qui calcule la moyenne de plusieurs nombres.
+
+        // Affichez "Mon moyenneur super rapide - v0.1.0"
+
+        // Générez un ensemble de 12 nombres aléatoires à l'aide de votre méthode et
+        // stockez ce travail dans une variable nommée 'nombres'.
+
+        // Affichez "Liste des nombres générés : " suivi de chacun de ces
+        // nombres sur la même ligne à la suite. Pour terminer passez à la ligne
+        // suivante sur la console.
+
+        // Calculer la moyenne des nombres aléatoires générés.
+
+        // Affichez la moyenne obtenue sous la forme "Moyenne de ces valeurs : xyz"
+
+    }
+
+}
